f881954f18cbf865d4e55f3b5a1c4fd9e6733f0c
masterj <masterj122517@163.com>
2022-12-12 00:08:27 +0800

    emacs (masterj's)

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,43 @@
+(setq inhibit-startup-screen t)
+
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+
+(setq package-check-signature nil)
+(require 'package)
+
+(unless (bound-and-true-p package-initalized)
+  (package-initialize))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+  
+(eval-and-compile)
+(setq use-package-always-ensure t)
+(setq use-package-always-defer t)
+(setq use-package-always-demand nil)
+(setq use-package-expand-minimally t)
+(setq use-package-verbose t)
+;; Appearance
+(use-package gruber-darkder
+    :init (load-theme 'gruber-darker t))
+(set-frame-font "Iosevka-20" nil t)
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(column-number-mode 1)
+(show-paren-mode 1)
+
+;; Evil Mode 
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
