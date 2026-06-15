e46d8e98c9303a11af4634d4b4e92832fb32d573
masterj <masterj122517@163.com>
2026-06-15 21:40:35 +0800

    fix: remove some unused

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -5,7 +5,7 @@
 # --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
 # --color=selected-bg:#45475A \
 # --color=border:#6C7086,label:#CDD6F4"
-#
+
 # background transparent
 export FZF_DEFAULT_OPTS=" --bind=ctrl-t:top,change:top --bind ctrl-j:down,ctrl-k:up \
 --color=bg+:#313244,bg:-1,spinner:#F5E0DC,hl:#F38BA8 \
